5037ee0d214832840173874ccfa692031333709b
joaodev <joaopcinpythonbotjoaopc@gmail.com>
2026-09-20 20:53:27 +0100

    Add new map options to the combobox

--- Main_Scripts.py
+++ Main_Scripts.py
@@ -8,9 +8,9 @@ root.geometry("400x250")
 root.title("need map name to find suspect")
 root.resizable(False, False)
 
-map_name = ["Motel Room and Humble Abode map", "Bathroom map"]
+map_name = ["Motel Room and Humble Abode map", "Bathroom map","Gas Station map","Abandoned Apartment map"]
 
-combobox1 = Combobox(root,values=map_name,state="readonly",width=30)
+combobox1 = Combobox(root,values=map_name,state="readonly",width=34)
 combobox1.place(x=20, y=20)
 
 bt1 = Button(root,text="Find Suspects",command=lambda: run(combobox1.get()))
@@ -29,6 +29,10 @@ def run(map_name):
         root.destroy()
         import Gas_Station_map
         Gas_Station_map.run()
+    elif map_name == "Abandoned Apartment map":
+        root.destroy()
+        import Abandoned_Apartment_map
+        Abandoned_Apartment_map.run()
     else:
         messagebox.showwarning("Invalid Map","Please select a valid map name.")
 
