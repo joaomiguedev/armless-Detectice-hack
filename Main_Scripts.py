7b670d24298acbfedbb1ad318ed58b9b4faa66a3
joaodev <joaopcinpythonbotjoaopc@gmail.com>
2026-09-20 22:09:41 +0100

    Add 'Moolah Manor map' to map selection

--- Main_Scripts.py
+++ Main_Scripts.py
@@ -8,7 +8,7 @@ root.geometry("400x250")
 root.title("need map name to find suspect")
 root.resizable(False, False)
 
-map_name = ["Motel Room and Humble Abode map", "Bathroom map","Gas Station map","Abandoned Apartment map"]
+map_name = ["Motel Room and Humble Abode map", "Bathroom map","Gas Station map","Abandoned Apartment map","Moolah Manor map"]
 
 combobox1 = Combobox(root,values=map_name,state="readonly",width=34)
 combobox1.place(x=20, y=20)
@@ -33,6 +33,10 @@ def run(map_name):
         root.destroy()
         import Abandoned_Apartment_map
         Abandoned_Apartment_map.run()
+    elif map_name == "Moolah Manor map":
+        root.destroy()
+        import Moolah_Manor_map
+        Moolah_Manor_map.run()
     else:
         messagebox.showwarning("Invalid Map","Please select a valid map name.")
 
